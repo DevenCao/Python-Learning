094cbf8f887bdfab87018d56478455cf263a7344
DevenCao <87301444+DevenCao@users.noreply.github.com>
2021-07-12 05:23:37 -0700

    Update list_overlap.py

--- exercises/list_overlap.py
+++ exercises/list_overlap.py
@@ -12,10 +12,18 @@ Extras:
 2.Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
 '''
 
+from random import  sample
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-newlist = [for item in a if item in b]
-print(newlist)
+print(list({item for item in a if item in b}))
 
 #extra-1
+a = sample(range(100), 20)
+b = sample(range(100), 20)
+print(a, '\n', b)
+print(list(set(a).intersection(set(b))))
+
+#extra-2
+print(list(set(sample(range(100), 20)).intersection(set(sample(range(100), 20)))))
+
