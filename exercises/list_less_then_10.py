dfc79861cd3b8f8a2a9f288040760e6b2bb66799
DevenCao <87301444+DevenCao@users.noreply.github.com>
2021-07-12 03:24:39 -0700

    Update list_less_then_10.py

--- exercises/list_less_then_10.py
+++ exercises/list_less_then_10.py
@@ -12,20 +12,17 @@ Extras:
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 #iterate list a and print out elements les than 5
-for ele in a:
-  if ele < 5:
-    print(ele)
+print([e for e in a if e < 5])
 
-#iterate list a then add elements less than 5 to a new list b
-b = []
-for ele in a:
-  if ele < 5:
-    b.append(ele)
-print(b)
+#iterate list a then add elements less than 5 to a new list
+newlist = [e for e in a if e < 5]
+print(newlist)
 
 #extra-2
-print(list(filter(lambda x:(x<5), a)))
+#print(list(filter(lambda x:(x<5), a)))
+print([e for e in a if e < 5])
 
 #extra-3
 number = int(input("Enter a number: "))
-print(list(filter(lambda x:(x < number), a)))
+#print(list(filter(lambda x:(x < number), a)))
+print([e for e in a if e < number])
