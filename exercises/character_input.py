dce8fe939ab05359ebe32f7080ab94a269efb755
DevenCao <87301444+DevenCao@users.noreply.github.com>
2021-07-12 02:23:34 -0700

    Correct syntax errors

--- exercises/character_input.py
+++ exercises/character_input.py
@@ -1,20 +1,19 @@
-/*
-Create a program that asks the user to enter their name and their age. 
-Print out a message addressed to them that tells them the year that they 
+'''
+Create a program that asks the user to enter their name and their age.
+Print out a message addressed to them that tells them the year that they
 will turn 100 years old.
-*/
+'''
 
 #Input name & age
 name = input("Please enter your name: ")
 print("Your name is " + name)
 age = input("Please enter your age: ")
+print("Your age is " + age)
 age = int(age)
-print("You age is " + age)
 
 #calculate the year age will become 100
 
 year = 100 - age + 2021
 
-print("You will turn 100 at " + year)  
+print("You will turn 100 at " + str(year))
 
-input()
